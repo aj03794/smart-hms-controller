0d8fc9a4a6b2d96b7a54dad087e2c719478e98b0
aj03794 <adamjohnston151@yahoo.com>
2018-06-03 12:17:40 -0400

    Adding webpack config file

---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+const path = require('path')
+const webpack = require('webpack')
+const CleanWebpackPlugin = require('clean-webpack-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+
+module.exports = {
+	entry: './src/index',
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		filename: 'bundle.js',
+	},
+	resolve: {
+		alias: {
+			'hiredis': path.join(__dirname, 'aliases/hiredis.js')
+		}
+	},
+	plugins: [
+		new webpack.optimize.LimitChunkCountPlugin({
+			maxChunks: 1,
+		}),
+		new CleanWebpackPlugin(['./dist']),
+	],
+	module: {
+		'rules': [
+			{
+				'loader': 'babel-loader',
+				'test': /\.js$/,
+				'exclude': /node_modules/,
+				'options': {
+					'plugins': [
+						require('babel-plugin-syntax-dynamic-import'),
+						require('babel-plugin-transform-object-rest-spread')
+					],
+					'presets': [
+						'env'
+					],
+					babelrc: false,
+					compact: false
+				}
+			}
+		]
+	},
+	target: 'node',
+	node: {
+		__dirname: false,
+		__filename: false
+	}
+}
