faa1392787d76e75be70cde73829f564e0cf35f9
aj03794 <adamjohnston151@yahoo.com>
2018-06-12 15:24:45 -0400

    Setting up redis host variable

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ import { redis } from './redis'
 import { slack as slackCreator } from './slack'
 import { initAppController } from './app-controller'
 
-const { publisherCreator, subscriberCreator } = redis()
+const { publisherCreator, subscriberCreator } = redis({
+	host: process.env[2] === 'dev' ? '127.0.0.1' : 'main.local'
+})
 
 Promise.all([
 	publisherCreator(),
