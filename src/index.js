71aab5be213fdd3020a99ed622fa5b607f8960d2
aj03794 <adamjohnston151@yahoo.com>
2018-06-25 19:34:05 -0400

    Refactoring setup

--- src/index.js
+++ src/index.js
@@ -3,38 +3,24 @@ import RPI from 'rpi-info'
 import { redis } from './redis'
 import { slack as slackCreator } from './slack'
 import { initAppController } from './app-controller'
-
+import { getModel } from './raspberry-pi'
 
 const { publisherCreator, subscriberCreator } = redis()
 
-// console.log('asdfas', rpi.getRevision())
-
-
 Promise.all([
 	publisherCreator(),
-	subscriberCreator()
+	subscriberCreator(),
+	getModel({ RPI })
 ])
 .then(([
 	{ publish },
-	{ subscribe }
+	{ subscribe },
+	{ model }
 ]) => {
 	const slack = slackCreator({ publish })
-
-	let revision
-
-	try {
-		const rpi = new RPI()
-		revision = rpi.getModel()
-	}
-	catch (e) {
-		if (e.code === 'ENOENT' && e.path === '/proc/cpuinfo') {
-			console.log('This is dev')
-			revision = 'development'
-		}
-	}
 	return initAppController({
 		publish,
 		subscribe,
-		revision
+		model
 	})
 })
